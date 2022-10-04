afe283f33c64c05f2bcf831386907a25902fd2d4
corbin419 <corbin50111@gmail.com>
2022-10-04 16:37:44 +0800

    Gototop  btn ok

--- src/Components/GotoTop.js
+++ src/Components/GotoTop.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../CSS/GotoTop.css";
 import { AiOutlineArrowUp } from "react-icons/ai";
+import { useScrollPosition } from "../Hooks/useScrollPosition";
 
 export default function GotoTop() {
+  const [Show, setShow] = useState("GotoTop");
+  const scrollPosition = useScrollPosition();
 
+  useEffect(() => {
+    if (scrollPosition > 20) {
+      setShow("GotoTop");
+    } else {
+      setShow("GotoTop Show");
+    }
+  }, [scrollPosition]);
   return (
-    <button className="GotoTop">
-      <AiOutlineArrowUp />
-    </button>
+    <a href="/#Home">
+      <button className={Show}>
+        <AiOutlineArrowUp />
+      </button>
+    </a>
   );
 }
